f323fc2ea555b057e22d5983ebe6e5c7c4afba54
lsh382510118 <382510118@qq.com>
2016-10-08 10:13:12 +0800

    point
    canvasPoint  and  divPoint pull

--- js/commonJs/common.js
+++ js/commonJs/common.js
@@ -2,7 +2,6 @@ function tabFunc(obj,index){
     var tabParentId = obj.parentNode.id;
     var tabList = document.getElementById(tabParentId).getElementsByTagName('div');
     console.info(tabParentId);
-
     for (var i =0;i<tabList.length;i++){
         if(index == i){
             tabList[i].className = 'active';
--- js/plugIn/plugIn.js
+++ js/plugIn/plugIn.js
@@ -9,4 +9,10 @@ $(function(){
         $('.layer-mark').removeClass('in').removeClass('out').addClass('out');
         setTimeout(function(){$('.artical-layer-wrap').remove();$('.layer-box').hide();},700)
     })
+    var btnEnlarge = document.getElementById('btnEnlarge');
+    function openHtml(){
+        var PlugInhHtmlPage = document.getElementById('PlugInhHtmlPage');
+        var link = PlugInhHtmlPage.getAttribute('src');
+        window.open(link, 'newwindow', 'height=600, width=800, top=0, left=0, toolbar=no, menubar=no');
+    }
 });
