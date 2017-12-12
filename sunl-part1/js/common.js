4dddce83ce5d8d50f95b7277441137c56f75baf7
lsh382510118 <382510118@qq.com>
2017-12-12 17:10:26 +0800

    sun-dk

---
+++ sunl-part1/js/common.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/12/6.
+ */
+ // fooer
+$('.footer-nav-item').click(function () {
+    $(this).addClass('active').siblings().removeClass('active');
+});
+
---
+++ sunl-part1/js/rem.js
@@ -0,0 +1,14 @@
+(function (doc, win) {
+  var docEl = doc.documentElement,
+    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function () {
+      var clientWidth = docEl.clientWidth;
+      if (!clientWidth) return;
+      docEl.style.fontSize = 40 * (clientWidth / 750) + 'px';
+      // console.log(40 * (clientWidth / 1080))
+    };
+
+  if (!doc.addEventListener) return;
+  win.addEventListener(resizeEvt, recalc, false);
+  doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
